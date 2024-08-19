91649a21689b09098a39a4c4eb037ea23a7eb1c7
Mohit Yadav <90053172+mohit1465@users.noreply.github.com>
2024-08-19 22:05:18 +0530

    Update QR code data

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-{"13sxew":"scanned","ret34":"unscanned","xrrde5":"scanned","http://en.m.wikipedia.org":"scanned","mohit1465":"unscanned"}
+{"13sxew":"scanned","ret34":"unscanned","xrrde5":"scanned","http://en.m.wikipedia.org":"scanned","mohit1465":"scanned"}
