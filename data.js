08e1fe60bf545d7fdafd5b78589fa773262acd29
Mohit Yadav <90053172+mohit1465@users.noreply.github.com>
2024-08-19 22:02:21 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,6 @@
     "13sxew": "scanned",
     "ret34": "unscanned",
     "xrrde5": "scanned",
-    "http://en.m.wikipedia.org": "unscanned"
+    "http://en.m.wikipedia.org": "unscanned",
     "mohit1465": "unscanned"
 }
